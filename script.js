4a1848ee5dc4df9280ccf4a13e04af6cd8f26522
HydraaX33 <andrzejlepiarz019@gmail.com>
2026-03-15 14:25:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Inicjalizacja animacji pojawiania się
-AOS.init({
-    duration: 1000,
-    once: false, // Animacja powtarza się przy każdym skrolowaniu
-});
 
-// Płynne przewijanie menu
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        if(this.getAttribute('href').startsWith('#')) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            target.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
